ad2b1854443380315fb871f6a2b12173da6b1e2f
Harouna Traoré <haroun@users.noreply.github.com>
2024-09-12 10:52:26 +0200

    update diagnostics icons and setup lint

--- nvim/lua/plugins/diagnostics.lua
+++ nvim/lua/plugins/diagnostics.lua
@@ -1,3 +1,29 @@
+-- diagnostics icons
+-- Sort diagnostics by severity and display highest severity first
+-- Highlight entire line for errors
+-- Highlight the line number for warnings
+vim.diagnostic.config({
+  severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "*",
+      [vim.diagnostic.severity.WARN] = "!",
+      [vim.diagnostic.severity.INFO] = "&",
+      [vim.diagnostic.severity.HINT] = "?",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
+    },
+  },
+})
+-- vim.fn.sign_define("DiagnosticSignError", { text = "*", texthl = "DiagnosticSignError" })
+-- vim.fn.sign_define("DiagnosticSignWarn", { text = "!", texthl = "DiagnosticSignWarn" })
+-- vim.fn.sign_define("DiagnosticSignInfo", { text = "&", texthl = "DiagnosticSignInfo" })
+-- vim.fn.sign_define("DiagnosticSignHint", { text = "?", texthl = "DiagnosticSignHint" })
+
 return {
   "folke/trouble.nvim",
   opts = { -- for default options, refer to the configuration section for custom setup.
---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,23 @@
+return {
+  "mfussenegger/nvim-lint",
+  config = function()
+    require("lint").linters_by_ft = {
+      text = { "markdownlint" },
+      markdown = { "markdownlint" },
+      javascript = { "eslint_d" },
+      json = { "jsonlint" },
+      css = { "stylelint" },
+      scss = { "stylelint" },
+      sass = { "stylelint" },
+      less = { "stylelint" },
+      yaml = { "yamllint" },
+    }
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        -- try_lint without arguments runs the linters defined in `linters_by_ft`
+        -- for the current filetype
+        require("lint").try_lint()
+      end,
+    })
+  end,
+}
