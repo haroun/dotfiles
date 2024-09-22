eb019a7f2f09123660c6dd72721765a94eb22c30
Harouna Traoré <haroun@users.noreply.github.com>
2024-09-22 11:14:41 +0200

    all folds are open by default

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,7 +2,7 @@
 -- NOTE: see `:help vim.g`
 vim.g.mapleader = " " -- used for mappings as <leader>
 vim.g.maplocalleader = " " -- used for mappings as <localleader> which are local to a buffer
-vim.g.have_nerd_font = true
+-- vim.g.have_nerd_font = true
 
 -- options
 -- NOTE: see `:help vim.opt`
@@ -10,8 +10,9 @@ vim.opt.number = true -- print line number
 vim.opt.relativenumber = true -- print relative line number
 vim.opt.termguicolors = true -- enable 24-bit rgb color
 
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldmethod = "expr" -- 'foldexpr' gives the fold level of a line
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- evaluated for each line to obtain its fold level
+vim.wo.foldenable = "off" -- all folds are open
 
 -- NOTE: see `:help modeline`
 -- vim: tabstop=2 softtabstop=2 shiftwidth=2 expandtab
