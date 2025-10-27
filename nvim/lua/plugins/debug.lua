43ec633df405539f14ec458080a9dd63284cef05
Harouna Traoré <haroun@users.noreply.github.com>
2025-10-27 15:26:07 +0100

    update with kickstart.nvim #3338d39

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- NOTE: Yes, you can install new plugins here!
     "mfussenegger/nvim-dap",
     -- NOTE: And you can specify dependencies as well
     dependencies = {
@@ -10,11 +11,11 @@ return {
       "nvim-neotest/nvim-nio",
 
       -- Installs the debug adapters for you
-      "williamboman/mason.nvim",
+      "mason-org/mason.nvim",
       "jay-babu/mason-nvim-dap.nvim",
 
       -- Add your own debuggers here
-      "leoluz/nvim-dap-go",
+      -- "leoluz/nvim-dap-go",
     },
     keys = function(_, keys)
       local dap = require("dap")
@@ -82,6 +83,18 @@ return {
           },
         },
       })
+
+      -- Change breakpoint icons
+      -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+      -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+      -- local breakpoint_icons = vim.g.have_nerd_font
+      --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+      --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+      -- for type, icon in pairs(breakpoint_icons) do
+      --   local tp = 'Dap' .. type
+      --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+      -- end
 
       dap.listeners.after.event_initialized["dapui_config"] = dapui.open
       dap.listeners.before.event_terminated["dapui_config"] = dapui.close
--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,15 +1,15 @@
 return {
   { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      local lint = require 'lint'
+      local lint = require("lint")
       lint.linters_by_ft = {
         css = { "stylelint" },
         javascript = { "eslint_d" },
         json = { "jsonlint" },
         less = { "stylelint" },
-        markdown = { 'markdownlint' },
+        markdown = { "markdownlint" },
         sass = { "stylelint" },
         scss = { "stylelint" },
         text = { "markdownlint" },
@@ -50,11 +50,16 @@ return {
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         group = lint_augroup,
         callback = function()
-          lint.try_lint()
+          -- Only run the linter in buffers that you can modify in order to
+          -- avoid superfluous noise, notably within the handy LSP pop-ups that
+          -- describe the hovered symbol using Markdown.
+          if vim.bo.modifiable then
+            lint.try_lint()
+          end
         end,
       })
     end,
