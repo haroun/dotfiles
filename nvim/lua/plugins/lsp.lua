8ea52bb80ff0a3c78fbddb28808b6fc4638047e2
Harouna Traoré <haroun@users.noreply.github.com>
2025-10-27 15:50:23 +0100

    vue setup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -227,6 +227,44 @@ return {
       --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require("blink.cmp").get_lsp_capabilities()
 
+      -- Vue setup
+      -- https://github.com/vuejs/language-tools/wiki/Neovim
+      local vue_language_server_path = vim.fn.expand("$MASON/packages")
+        .. "/vue-language-server"
+        .. "/node_modules/@vue/language-server"
+      local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+      local vue_plugin = {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+        configNamespace = "typescript",
+      }
+      local vtsls_config = {
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                vue_plugin,
+              },
+            },
+          },
+        },
+        filetypes = tsserver_filetypes,
+      }
+      local ts_ls_config = {
+        init_options = {
+          plugins = {
+            vue_plugin,
+          },
+        },
+        filetypes = tsserver_filetypes,
+      }
+      local vue_ls_config = {}
+      -- vim.lsp.config('vtsls', vtsls_config)
+      -- vim.lsp.config('vue_ls', vue_ls_config)
+      -- vim.lsp.config('ts_ls', ts_ls_config)
+      -- vim.lsp.enable({'vtsls', 'vue_ls'}) -- If using `ts_ls` replace `vtsls` to `ts_ls`
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -282,24 +320,10 @@ return {
         },
         markdown_oxide = {},
         tailwindcss = {},
-        ts_ls = {
-          root_dir = require("lspconfig").util.root_pattern("package.json"),
-          single_file_support = false,
-
-          -- https://github.com/vuejs/language-tools?tab=readme-ov-file#hybrid-mode-configuration-requires-vuelanguage-server-version-200
-          init_options = {
-            plugins = {
-              {
-                name = "@vue/typescript-plugin",
-                location = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server"),
-                languages = { "vue" },
-              },
-            },
-          },
-          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-        },
+        ts_ls = ts_ls_config,
         vimls = {},
-        vue_ls = {},
+        vtsls = vtsls_config,
+        vue_ls = vue_ls_config,
         yamlls = {},
       }
 
@@ -360,6 +384,9 @@ return {
           end,
         },
       })
+
+      -- TODO: how to do the following?
+      -- vim.lsp.enable({'vtsls', 'vue_ls'}) -- If using `ts_ls` replace `vtsls` to `ts_ls`
     end,
   },
 }
